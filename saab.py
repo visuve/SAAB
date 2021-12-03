124cf3a0f4a5b88bf9384d76940b938e6bcae68d
visuve <veli-matti.visuri@iki.fi>
2021-12-03 18:18:42 +0200

    Add .pylintrc & cleanup code

--- saab.py
+++ saab.py
@@ -11,24 +11,28 @@ from PyQt6.QtSql import QSqlDatabase, QSqlQueryModel
 
 class Saab(QMainWindow):
     def __init__(self):
-        super(Saab, self).__init__()
+        super().__init__()
 
         uic.loadUi(QDir.currentPath() + "/saab.ui", self)
 
         self.action_open.triggered.connect(self.open_database)
         self.button_query.clicked.connect(self.query_database)
-        self.action_exit.triggered.connect(self.exit_application)
+        self.action_exit.triggered.connect(QCoreApplication.exit)
 
         self.database = None
 
     def open_database(self):
-        database_path = QFileDialog.getOpenFileName(self, "Open SQLite database", QDir.homePath(), "SQLite database (*.sqlite)")[0];
+        database_path = QFileDialog.getOpenFileName(
+            self,
+            "Open SQLite database",
+            QDir.homePath(),
+            "SQLite database (*.sqlite)")[0]
 
         if not database_path:
             return
 
-        self.database = QSqlDatabase.addDatabase("QSQLITE");
-        self.database.setDatabaseName(database_path);
+        self.database = QSqlDatabase.addDatabase("QSQLITE")
+        self.database.setDatabaseName(database_path)
 
         if not self.database.open():
             print(self.database.lastError())
@@ -41,13 +45,9 @@ class Saab(QMainWindow):
 
         self.table_view.setModel(query_model)
 
-    def exit_application(self):
-        QCoreApplication.exit()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Saab()
     window.show()
     sys.exit(app.exec())
-
